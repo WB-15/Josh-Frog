c40fcf6d42ae29b87cf5cebeaf65600f1c81ad1d
SeniorDev-1005 <seniordev1005@gmail.com>
2021-02-24 09:57:52 -0500

    Adds Windows build with simple C++ plugin

--- electron/preloader.js
+++ electron/preloader.js
@@ -1,2 +1,2 @@
 require('./node_modules/@capacitor-community/electron/dist/electron-bridge.js');
-require('./node_modules/capacitor-mettler-toledo/electron/dist/plugin');
+require('./node_modules/capacitor-mettler-toledo/electron/dist/plugin.js');
