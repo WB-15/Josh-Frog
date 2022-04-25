8cfa029b8f3372b8315b607a169a5fccd874f959
Cory Madaj <cory.madaj@joshsfrogs.com>
2022-04-25 11:13:24 -0400

    COGS-247: Route user to login if their authentication has expired

--- src/app/modules/shared/services/auth-guard.service.ts
+++ src/app/modules/shared/services/auth-guard.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate } from '@angular/router';
 import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(private userService: UserService) {}
 
   canActivate() {
     if (!this.userService.isAuthenticated()) {
-      this.router.navigate(['/account/login']);
+      this.userService.logout(true);
       return false;
     }
     return true;
