c2b1ea1eb10329e71c144730910879a09cb354de
Cory Madaj <cory.madaj@joshsfrogs.com>
2022-06-17 12:08:26 -0400

    COGS-176: Updated pagination to newer version from client. Only difference is the active page is blue, not green

--- src/app/modules/shared/components/pagination/pagination.component.ts
+++ src/app/modules/shared/components/pagination/pagination.component.ts
@@ -1,22 +1,84 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, EventEmitter, Inject, Input, OnDestroy, OnInit, Output, PLATFORM_ID } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
-  styles: []
+  styleUrls: ['./pagination.component.scss']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit, OnDestroy {
+  _pageSize: number;
+  @Input() set pageSize(pageSize: number) {
+    this._pageSize = pageSize;
+    this.calculatePageCount();
+  }
+
+  _count: number;
+  @Input() set count(count: number) {
+    this._count = count;
+    this.calculatePageCount();
+  }
+
+  _pageOffset = 2;
+  @Input() set pageOffset(pageOffset: number) {
+    this._pageOffset = pageOffset;
+    this.pageOffsetButtons = new Array(pageOffset);
+  }
+
   @Input() page: number;
-  @Input() pageSize: number;
-  @Input() count: number;
 
   @Output() pageChanged = new EventEmitter<number>();
 
-  constructor() {}
+  isBrowser = isPlatformBrowser(this.platformId);
+  pageCount: number;
+  math = Math;
+  pageOffsetButtons = new Array(2);
+  widthResizeSubscription: Subscription;
 
-  ngOnInit(): void {}
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      this.setPageOffset();
+      this.widthResizeSubscription = fromEvent(window, 'resize')
+        .pipe(throttleTime(200, undefined, { trailing: true }))
+        .subscribe((event) => {
+          this.setPageOffset();
+        });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.isBrowser) {
+      this.widthResizeSubscription.unsubscribe();
+    }
+  }
+
+  setPageOffset() {
+    const newWidth = window.innerWidth;
+    if (newWidth < 1024 && this._pageOffset !== 1) {
+      this.pageOffset = 1;
+    }
+
+    if (newWidth >= 1024 && this._pageOffset < 2) {
+      this.pageOffset = 2;
+    }
+  }
 
   changePage(index: number) {
-    this.pageChanged.emit(Math.floor(index));
+    const newPage = Math.floor(index);
+    if (newPage !== this.page) {
+      this.pageChanged.emit(newPage);
+    }
+  }
+
+  calculatePageCount() {
+    if (this._pageSize && this._count) {
+      this.pageCount = Math.ceil(this._count / this._pageSize);
+    } else {
+      this.pageCount = 1;
+    }
   }
 }
